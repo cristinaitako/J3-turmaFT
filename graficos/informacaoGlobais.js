c4d6a8547e1958b8e4b7a56865c1ff6fdce658fb
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-10-30 20:30:08 +0000

    lloooo

--- graficos/informacaoGlobais.js
+++ graficos/informacaoGlobais.js
@@ -4,7 +4,6 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
-
 const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML ='Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
