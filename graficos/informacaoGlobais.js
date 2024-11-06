ae9425affd77c0b22e2c7ff96b4b281abccb4eff
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-11-06 20:35:56 +0000

    jhhh

--- graficos/informacaoGlobais.js
+++ graficos/informacaoGlobais.js
@@ -8,7 +8,8 @@ async function vizualizarInformacoesGlobais() {
     const horas = parseInt(dados.tempo_medio_trabalho_por_semana)
     const minutos = Math.round((dados.tempo_medio_trabalho_por_semana-horas)*60)
     const porcentagemConectadas =((pessoasEmpregadas/pessoasNoMundo)*100).toFixed(2)
-const paragrafo = document.createElement('p')
+
+    const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML =`Você sabia que o mundo tem <span> ${pessoasNoMundo} </span> de pessoas e que aproximadamente <span> ${pessoasEmpregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${horas} </span> horas e <span> ${minutos} </span> minutos por semana.<br> Isso que significa que aproximandamente <span> ${porcentagemConectadas}% </span> pessoas conectadas em alguma rede social.`
     const container = document.getElementById('graficos-container')
