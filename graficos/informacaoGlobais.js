c7b927421cfc4a2e502337f7dff7a81954f6a3fb
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-11-06 20:07:20 +0000

    informmmss

--- graficos/informacaoGlobais.js
+++ graficos/informacaoGlobais.js
@@ -3,7 +3,11 @@ const url='https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicos
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    const pessoasConectadas = (dados.total_pessoas_com_acesso_a_trabalho/1e9) 
+    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9) 
+    const horas = parseInt(dados.tempo_medio_dia_trabalhando)
+    const minutos = Math.round((dados.tempo_medio_dia_trabalhando - horas)*60)
+    
 const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML =`Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
