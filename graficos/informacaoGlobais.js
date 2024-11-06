3d89e82a956a054884a56153bec862fd680b027e
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-11-06 20:18:28 +0000

    kkkk

--- graficos/informacaoGlobais.js
+++ graficos/informacaoGlobais.js
@@ -3,17 +3,16 @@ const url='https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicos
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_com_acesso_a_trabalho/1e9) 
+    const pessoasEmpregadas = (dados.total_pessoas_empregadas/1e9) 
     const pessoasNoMundo = (dados.total_pessoas_mundo/1e9) 
-    const horas = parseInt(dados.tempo_medio_dia_trabalhando)
-    const minutos = Math.round((dados.tempo_medio_dia_trabalhando - horas)*60)
+    const horas = parseInt(dados.tempo_medio_trabalho_por_semana)
+    const minutos = Math.round((dados.tempo_medio_trabalho_por_semana-horas)*60)
     
 const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML =`Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+    paragrafo.innerHTML =`Você sabia que o mundo tem <span> ${pessoasNoMundo} </span> de pessoas e que aproximadamente <span> ${pessoasEmpregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${horas} </span> horas e <span> ${minutos} </span> minutos por semana.` 
     const container = document.getElementById('graficos-container')
 container.appendChild(paragrafo);
 
 }
-
 vizualizarInformacoesGlobais()
