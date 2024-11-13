007c1283e3f6b3a67ee8a72155d3cb53283c6771
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-11-13 20:38:12 +0000

    kjhhh

--- graficos/quantidadeTrabalhadores.js
+++ graficos/quantidadeTrabalhadores.js
@@ -11,49 +11,14 @@ async function quantidadeTrabalhadores() {
             x: nomeTrabalhadores, 
             y: quantidadeTrabalhadores, 
             type: 'bar',
-            marker: {
-                color: getCSS('--primary-color')
-            }
+        ]
         }
-    ]
-
-    const laytout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        title: {
-            text: 'Relatórios de pessoas trabalhando',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
-            }
-        },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nome dos profissionais,
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Bilhões profissonais ativos',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        }
-    }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
     Plotly.newPlot(grafico, data, laytout)
-}
+         
 
 quantidadeTrabalhadores()
       
