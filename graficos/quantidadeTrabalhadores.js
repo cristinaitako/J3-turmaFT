5b0521cf8fad412c7b08362210dc958e952c8089
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2024-11-13 20:46:53 +0000

    lkkkk

--- graficos/quantidadeTrabalhadores.js
+++ graficos/quantidadeTrabalhadores.js
@@ -4,16 +4,16 @@ async function quantidadeTrabalhadores() {
     const res = await fetch(url)
     const dados = await res.json()
     const nomeTrabalhadores = Object.keys(dados)
-    const quantidadeDeTrabalhadores= Object.values(dados)
+    const quantidadeTrabalhadores= Object.values(dados)
 
     const data = [
         {
             x: nomeTrabalhadores, 
             y: quantidadeTrabalhadores, 
             type: 'bar',
-        ]
-        }
-
+        }   
+    ]
+        
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
